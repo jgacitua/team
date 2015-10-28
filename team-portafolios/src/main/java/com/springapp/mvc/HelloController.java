e46fa682c7e633ac5f3384f6269e2646d0c0074d
jgacitua <jgacitua.ing@gmail.com>
2015-10-28 20:14:15 +0100

    probando slide de presentacion team
    probando slide de presentacion team, aunque lo termine eliminando

---
+++ team-portafolios/src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,15 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HelloController {
+	@RequestMapping(value = "/index")
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world! test");
+		return "hello";
+	}
+}
